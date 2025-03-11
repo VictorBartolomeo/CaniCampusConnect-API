4cf7bc017517f63d4c7b42194db184e785011a41
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-03-11 14:14:07 +0100

    add inheritance for Coach model

--- src/main/java/org/example/canicampusconnectapi/model/Coach.java
+++ src/main/java/org/example/canicampusconnectapi/model/Coach.java
@@ -7,12 +7,6 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Coach {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    protected Long id;
-
-    protected String last_name;
-    protected String first_name;
+public class Coach extends User{
 
 }
