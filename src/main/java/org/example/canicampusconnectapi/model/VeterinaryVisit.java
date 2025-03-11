39b72d5a5995df412d2cefc828187ced8e723725
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-03-11 14:19:28 +0100

    create Veterinary visit model

---
+++ src/main/java/org/example/canicampusconnectapi/model/VeterinaryVisit.java
@@ -0,0 +1,28 @@
+package org.example.canicampusconnectapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Time;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+public class VeterinaryVisit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    protected Date visit_date;
+    @Column(columnDefinition = "TEXT")
+    protected String diagnosis;
+    protected String reason_for_visit;
+
+    @Column(columnDefinition = "TEXT")
+    protected String treatment;
+
+    protected String veterinarian;
+}
