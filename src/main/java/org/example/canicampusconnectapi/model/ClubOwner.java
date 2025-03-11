b241f55e7c87b9a53ae47f70e274a69908954481
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-03-11 14:13:35 +0100

    create ClubOwner model

---
+++ src/main/java/org/example/canicampusconnectapi/model/ClubOwner.java
@@ -0,0 +1,12 @@
+package org.example.canicampusconnectapi.model;
+
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class ClubOwner extends User {
+}
