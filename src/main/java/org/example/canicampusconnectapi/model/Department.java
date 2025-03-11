c1272b1d0d26ab71fcf9b16736aaec8cb20344ce
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-03-11 14:15:19 +0100

    create Department model

---
+++ src/main/java/org/example/canicampusconnectapi/model/Department.java
@@ -0,0 +1,20 @@
+package org.example.canicampusconnectapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    protected String department_name;
+}
