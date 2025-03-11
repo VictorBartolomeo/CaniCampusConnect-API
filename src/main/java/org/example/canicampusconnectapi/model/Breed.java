447cd2bca7f949ebba71001ac09402de5de8bd5e
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-03-11 14:12:02 +0100

    create Breed model

---
+++ src/main/java/org/example/canicampusconnectapi/model/Breed.java
@@ -0,0 +1,24 @@
+package org.example.canicampusconnectapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Breed {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column( unique = true, nullable = false)
+    private String breed_name;
+
+    @OneToMany(mappedBy = "breed")
+    private List<Dog> dogs;
+
+}
