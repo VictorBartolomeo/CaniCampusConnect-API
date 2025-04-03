4ec4635bbb78a89447d16f41f4e4adfb3adbd59a
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-03 13:59:29 +0200

    add variables to child class Coach

--- src/main/java/org/example/canicampusconnectapi/model/Coach.java
+++ src/main/java/org/example/canicampusconnectapi/model/Coach.java
@@ -4,9 +4,15 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Entity
 @Getter
 @Setter
 public class Coach extends User{
+
+protected String acacedNumber;
+protected boolean isActive;
+protected Date registrationDate;
 
 }
