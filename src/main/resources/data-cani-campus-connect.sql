679aa00230d8e70c3a0583631d5af2ec950ace01
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-03 11:52:06 +0200

    add tests SQL db

---
+++ src/main/resources/data-cani-campus-connect.sql
@@ -0,0 +1,6 @@
+-- INSERT INTO user (firstname, lastname, email, password, phone)
+-- VALUES ('Victor','Monteragioni','victor.monteriggioni@gmail.com','CaniCampusAdmin','+33 6 02 02 49 53');
+
+
+INSERT INTO owner (firstname, lastname, email, password, phone,is_active,registration_date,address)
+VALUES ('Stéphane','Scheeres','stephane.scheeres@gmail.com','1234','0600000000',true,current_date,'Rue sainte Marie');
