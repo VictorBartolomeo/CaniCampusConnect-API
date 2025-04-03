c62840081453263d1dee0985cc2b16391e9b5918
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-03 13:59:46 +0200

    add firsts SQL queries

--- src/main/resources/data-cani-campus-connect.sql
+++ src/main/resources/data-cani-campus-connect.sql
@@ -1,6 +1,6 @@
--- INSERT INTO user (firstname, lastname, email, password, phone)
+INSERT INTO user (firstname, lastname, email, password, phone)
+VALUES ('Stéphane','Scheeres','stephane.scheeres@gmail.com','1234','0600000000');
 -- VALUES ('Victor','Monteragioni','victor.monteriggioni@gmail.com','CaniCampusAdmin','+33 6 02 02 49 53');
 
-
-INSERT INTO owner (firstname, lastname, email, password, phone,is_active,registration_date,address)
-VALUES ('Stéphane','Scheeres','stephane.scheeres@gmail.com','1234','0600000000',true,current_date,'Rue sainte Marie');
+INSERT INTO owner (id,is_active,registration_date,address)
+VALUES (1, true,current_date,'Rue sainte Marie');
