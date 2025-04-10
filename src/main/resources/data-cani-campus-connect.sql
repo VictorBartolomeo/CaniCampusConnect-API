18e1288dbfac8c7aa36a11d0dbcbb8484411b170
VictorBartolomeo <victor.monteragioni@icloud.com>
2025-04-10 23:12:42 +0200

    changing password for a hashed password

--- src/main/resources/data-cani-campus-connect.sql
+++ src/main/resources/data-cani-campus-connect.sql
@@ -1,6 +1,9 @@
 INSERT INTO user (firstname, lastname, email, password, phone)
-VALUES ('Stéphane','Scheeres','stephane.scheeres@gmail.com','1234','0600000000');
--- VALUES ('Victor','Monteragioni','victor.monteriggioni@gmail.com','CaniCampusAdmin','+33 6 02 02 49 53');
+VALUES ('Stéphane', 'Scheeres', 'stephane.scheeres@gmail.com', '$2a$10$BfXutvk76gOr/RblVNl7qOw3mDrBGxeiPWjS9B3/Iq4GgpwWIUODu', '0600000000'),
+       ('Victor', 'Monteragioni', 'victor.monteriggioni@gmail.com', '$2a$10$BfXutvk76gOr/RblVNl7qOw3mDrBGxeiPWjS9B3/Iq4GgpwWIUODu', '+33 6 02 02 49 53');
 
-INSERT INTO owner (id,is_active,registration_date,address)
-VALUES (1, true,current_date,'Rue sainte Marie');
+INSERT INTO owner (id, is_active, registration_date, address)
+VALUES (1, true, current_date, 'Rue sainte Marie');
+
+INSERT INTO club_owner(id)
+VALUES (2);
