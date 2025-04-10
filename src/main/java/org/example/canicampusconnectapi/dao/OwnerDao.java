9962474db4a169fefc486b1ed4757bc3c0c973b9
VictorBartolomeo <victor.monteragioni@icloud.com>
2025-04-10 21:29:54 +0200

    added findByEmail method to retrieve an owner

--- src/main/java/org/example/canicampusconnectapi/dao/OwnerDao.java
+++ src/main/java/org/example/canicampusconnectapi/dao/OwnerDao.java
@@ -1,11 +1,13 @@
 package org.example.canicampusconnectapi.dao;
 
-import org.example.canicampusconnectapi.model.Course;
 import org.example.canicampusconnectapi.model.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface OwnerDao extends JpaRepository<Owner, Long> {
+    Optional<Owner> findByEmail(String email);
 }
