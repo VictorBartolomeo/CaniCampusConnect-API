645e99689c1b927f857765fa8a4b96b59c0d6b82
VictorBartolomeo <victor.monteragioni@icloud.com>
2025-04-10 21:09:37 +0200

    add password encoder bean

--- src/main/java/org/example/canicampusconnectapi/CaniCampusConnectApiApplication.java
+++ src/main/java/org/example/canicampusconnectapi/CaniCampusConnectApiApplication.java
@@ -3,20 +3,28 @@ package org.example.canicampusconnectapi;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 public class CaniCampusConnectApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CaniCampusConnectApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CaniCampusConnectApiApplication.class, args);
+    }
 
-	@PostConstruct
-	public void init() {
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-	}
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
 
