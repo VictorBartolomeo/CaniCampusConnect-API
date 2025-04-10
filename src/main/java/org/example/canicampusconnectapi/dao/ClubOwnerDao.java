aaeaad7f7c1d9e08de857e23935a2fb35f28d13a
VictorBartolomeo <victor.monteragioni@icloud.com>
2025-04-10 21:30:03 +0200

    create club owner Dao

---
+++ src/main/java/org/example/canicampusconnectapi/dao/ClubOwnerDao.java
@@ -0,0 +1,13 @@
+package org.example.canicampusconnectapi.dao;
+
+import org.example.canicampusconnectapi.model.ClubOwner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface ClubOwnerDao extends JpaRepository<ClubOwner, Long> {
+    Optional<ClubOwner> findByEmail(String email);
+}
