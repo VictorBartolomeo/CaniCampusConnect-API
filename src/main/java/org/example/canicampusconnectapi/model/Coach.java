edfc0de60ff083bf72323fc22d194dc0c0713c7a
VictorBartolomeo <victor.monteragioni@icloud.com>
2025-04-16 23:13:57 +0200

    add annotations to fit the Merise MCD

--- src/main/java/org/example/canicampusconnectapi/model/Coach.java
+++ src/main/java/org/example/canicampusconnectapi/model/Coach.java
@@ -1,9 +1,11 @@
 package org.example.canicampusconnectapi.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDate;
 
@@ -11,10 +13,17 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @PrimaryKeyJoinColumn(name = "user_id")
-public class Coach extends User{
+public class Coach extends User {
 
-protected String acacedNumber;
-protected boolean isActive;
-protected LocalDate registrationDate;
+    @Column(nullable = true, length = 100)
+    protected String acacedNumber;
+
+    @Column(nullable = false)
+    @ColumnDefault("TRUE")
+    protected boolean isActive;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    protected LocalDate registrationDate;
 
 }
