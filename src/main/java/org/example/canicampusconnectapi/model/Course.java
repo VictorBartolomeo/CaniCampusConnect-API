06f75f152a576409464a955459d4d9d44fecc67e
Victor Monteragioni <victor.monteriggioni@gmail.com>
2025-04-28 18:51:08 +0200

    Refactor `Course` entity to add validation annotations, update datetime fields to `LocalDateTime`, and fix column mappings.

--- src/main/java/org/example/canicampusconnectapi/model/Course.java
+++ src/main/java/org/example/canicampusconnectapi/model/Course.java
@@ -1,9 +1,12 @@
 package org.example.canicampusconnectapi.model;
 
-
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -12,40 +15,36 @@ public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_id")
     protected Long id;
 
+    @Column(nullable = false, length = 255)
+    @NotBlank(message = "Le titre ne peut pas être vide")
     protected String title;
 
     @Column(columnDefinition = "TEXT")
     protected String description;
 
-    @Column(columnDefinition = "DATETIME")
-    protected String startDatetime;
-    @Column(columnDefinition = "DATETIME")
-    protected String endDatetime;
+    @Column(nullable = false)
+    @NotNull(message = "La date et l'heure de début ne peuvent pas être vides")
+    protected LocalDateTime startDatetime;
+
+    @Column(nullable = false)
+    @NotNull(message = "La date et l'heure de fin ne peuvent pas être vides")
+    protected LocalDateTime endDatetime;
 
     @Column(nullable = false)
     protected int maxCapacity;
-    protected int reservedCapacity;
-
-    @Column(nullable = false)
-    protected float price;
 
     @ManyToOne
-    @JoinColumn(name = "club_id"
-//            nullable = false
-    )
-    protected Club club;
-
-    @ManyToOne
-    @JoinColumn(name = "coach_id"
-//            nullable = false
-    )
+    @JoinColumn(name = "user_id", nullable = false)
     protected Coach coach;
 
     @ManyToOne
-    @JoinColumn(name = "course_type_id"
-//            nullable = false
-            )
+    @JoinColumn(name = "club_id", nullable = false)
+    protected Club club;
+
+    @ManyToOne
+    @JoinColumn(name = "course_type_id", nullable = false)
     protected CourseType courseType;
 }
