b2af3e88eb1d1ba794c9a8534003e04aefcd8b63
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-28 16:05:53 +0200

    Refactor Vaccination entity with enhanced field annotations

--- src/main/java/org/example/canicampusconnectapi/model/healthRecord/Vaccination.java
+++ src/main/java/org/example/canicampusconnectapi/model/healthRecord/Vaccination.java
@@ -14,22 +14,26 @@ public class Vaccination {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vaccination_id")
     protected Long id;
 
+    @Column(nullable = false)
     protected Date vaccinationDate;
+
+    @Column(nullable = true)
     protected Date reminderDate;
+
+    @Column(length = 255, nullable = true)
     protected String batchNumber;
+
+    @Column(length = 255, nullable = true)
     protected String veterinarian;
 
     @ManyToOne
-    @JoinColumn(nullable = false)
+    @JoinColumn(name = "dog_id", nullable = false)
     private Dog dog;
 
     @ManyToOne
     @JoinColumn(name = "vaccine_id", nullable = false)
     protected Vaccine vaccine;
-
-
-
-
 }
