2fc971b6881962e3ef328988daf6982d379ad25f
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-28 14:27:21 +0200

    Add DogWeightDao for accessing dog weight data

---
+++ src/main/java/org/example/canicampusconnectapi/dao/DogWeightDao.java
@@ -0,0 +1,17 @@
+package org.example.canicampusconnectapi.dao;
+
+import org.example.canicampusconnectapi.model.DogWeight;
+import org.example.canicampusconnectapi.model.Dog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Date;
+
+@Repository
+public interface DogWeightDao extends JpaRepository<DogWeight, Long> {
+    List<DogWeight> findByDog(Dog dog);
+    List<DogWeight> findByDogOrderByMeasurementDateDesc(Dog dog);
+    List<DogWeight> findByMeasurementDateBetween(Date startDate, Date endDate);
+    List<DogWeight> findByDogAndMeasurementDateBetween(Dog dog, Date startDate, Date endDate);
+}
