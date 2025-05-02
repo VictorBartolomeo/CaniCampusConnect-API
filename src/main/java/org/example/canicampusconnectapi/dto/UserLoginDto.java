f4215db806088803b93317d1ac87fd784775c136
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-02 11:09:16 +0200

    Add UserLoginDto for user login request validation

---
+++ src/main/java/org/example/canicampusconnectapi/dto/UserLoginDto.java
@@ -0,0 +1,18 @@
+package org.example.canicampusconnectapi.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class UserLoginDto {
+
+    @NotBlank(message = "L'email ne peut pas être vide")
+    @Email(message = "L'email n'est pas au format valide")
+    private String email;
+
+    @NotBlank(message = "Veuillez renseigner un mot de passe")
+    private String password;
+
+}
