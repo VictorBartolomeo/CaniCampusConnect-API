13b73f63acd1b40f88003408b7bdfdd2c2e02522
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-05 09:25:27 +0200

    Remove unused @Setter annotation from UserLoginDto.

--- src/main/java/org/example/canicampusconnectapi/dto/UserLoginDto.java
+++ src/main/java/org/example/canicampusconnectapi/dto/UserLoginDto.java
@@ -3,7 +3,6 @@ package org.example.canicampusconnectapi.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class UserLoginDto {
