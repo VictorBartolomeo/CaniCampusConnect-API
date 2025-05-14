87aaa670565eeae3b72030044e109786c9f4c08c
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-14 16:52:53 +0200

    Refactor `Breed` model to use `protected` fields and add `avatarUrl` attribute.

--- src/main/java/org/example/canicampusconnectapi/model/dogRelated/Breed.java
+++ src/main/java/org/example/canicampusconnectapi/model/dogRelated/Breed.java
@@ -1,8 +1,6 @@
 package org.example.canicampusconnectapi.model.dogRelated;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,14 +14,15 @@ public class Breed {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Short id;
+    protected Short id;
 
     @Column(length = 100, unique = true, nullable = false)
-    private String name;
+    protected String name;
 
     @ManyToMany(mappedBy = "breeds", fetch = FetchType.LAZY)
-    @JsonIgnoreProperties("breeds")
-    private Set<Dog> dogs;
+    protected Set<Dog> dogs;
+
+    String avatarUrl;
 
 
 }
