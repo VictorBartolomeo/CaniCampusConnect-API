8b946e9ae16ac8178d5e333604a4c0c9aa98e8a3
Victor Monteragioni <victor.monteriggioni@gmail.com>
2025-05-14 21:32:38 +0200

    Add CoachView, CoachViewCourse, and CoachViewRegistration classes with inheritance from owner views

---
+++ src/main/java/org/example/canicampusconnectapi/view/coach/CoachView.java
@@ -0,0 +1,6 @@
+package org.example.canicampusconnectapi.view.coach;
+
+import org.example.canicampusconnectapi.view.owner.OwnerView;
+
+public class CoachView extends OwnerView {
+}
---
+++ src/main/java/org/example/canicampusconnectapi/view/coach/CoachViewCourse.java
@@ -0,0 +1,6 @@
+package org.example.canicampusconnectapi.view.coach;
+
+import org.example.canicampusconnectapi.view.owner.OwnerViewCourse;
+
+public class CoachViewCourse extends OwnerViewCourse {
+}
