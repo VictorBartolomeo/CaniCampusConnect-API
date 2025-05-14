8e5ab1498a6d742d5bdc52883f44860fa0b3d972
Victor Monteragioni <victor.monteriggioni@gmail.com>
2025-05-14 22:18:21 +0200

    Add JsonView annotations to Course model for role-based serialization.

--- src/main/java/org/example/canicampusconnectapi/model/courseRelated/Course.java
+++ src/main/java/org/example/canicampusconnectapi/model/courseRelated/Course.java
@@ -2,6 +2,7 @@ package org.example.canicampusconnectapi.model.courseRelated;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -9,6 +10,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.example.canicampusconnectapi.model.users.Club;
 import org.example.canicampusconnectapi.model.users.Coach;
+import org.example.canicampusconnectapi.view.admin.AdminViewCourse;
+import org.example.canicampusconnectapi.view.owner.OwnerViewCourse;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -24,44 +27,49 @@ public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "course_id")
+    @JsonView(AdminViewCourse.class)
     protected Long id;
 
     @Column(nullable = false, length = 255)
     @NotBlank(message = "Le titre ne peut pas être vide")
+    @JsonView(OwnerViewCourse.class)
     protected String title;
 
     @Column(columnDefinition = "TEXT")
+    @JsonView(OwnerViewCourse.class)
     protected String description;
 
     @Column(nullable = false)
     @NotNull(message = "La date et l'heure de début ne peuvent pas être vides")
+    @JsonView(OwnerViewCourse.class)
     protected LocalDateTime startDatetime;
 
     @Column(nullable = false)
     @NotNull(message = "La date et l'heure de fin ne peuvent pas être vides")
+    @JsonView(OwnerViewCourse.class)
     protected LocalDateTime endDatetime;
 
     @Column(nullable = false)
+    @JsonView(OwnerViewCourse.class)
     protected int maxCapacity;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
-    @JsonManagedReference("coach-courses")
+    @JsonView(OwnerViewCourse.class)
     protected Coach coach;
 
     @ManyToOne
     @JoinColumn(name = "club_id", nullable = false)
-    @JsonManagedReference("club-courses")
+    @JsonView(OwnerViewCourse.class)
     protected Club club;
 
     @ManyToOne
     @JoinColumn(name = "course_type_id", nullable = false)
-    @JsonManagedReference("courseType-courses")
+    @JsonView(OwnerViewCourse.class)
     protected CourseType courseType;
 
     @OneToMany(mappedBy = "course")
-
-    @JsonManagedReference("course-registrations")
+    @JsonView(AdminViewCourse.class)
     protected List<Registration> registrations;
 
 }
