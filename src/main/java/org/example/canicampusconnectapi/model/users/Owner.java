6e683ca2c2b13eb801d18e7bfab522b0d2d514ac
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-15 12:05:43 +0200

    Refactor `Owner` model by removing unused imports and cleaning up annotations.

--- src/main/java/org/example/canicampusconnectapi/model/users/Owner.java
+++ src/main/java/org/example/canicampusconnectapi/model/users/Owner.java
@@ -1,14 +1,10 @@
 package org.example.canicampusconnectapi.model.users;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.example.canicampusconnectapi.model.dogRelated.Dog;
-import org.example.canicampusconnectapi.view.admin.AdminView;
-import org.example.canicampusconnectapi.view.coach.CoachView;
-import org.example.canicampusconnectapi.view.owner.OwnerView;
 import org.example.canicampusconnectapi.view.owner.OwnerViewDog;
 import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
@@ -26,18 +22,17 @@ public class Owner extends User {
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
-    @JsonView(OwnerViewDog.class)
+    @JsonView({OwnerViewDog.class})
     protected LocalDate registrationDate;
 
     @ColumnDefault("TRUE")
     @Column(nullable = false)
     protected boolean isActive;
 
-    @JsonView(OwnerViewDog.class)
+    @JsonView({OwnerViewDog.class})
     protected String address;
 
     @OneToMany(mappedBy ="owner")
-    //TODO Ici récursivité, comment l'éviter ?
     protected List<Dog> dogs;
 
 }
