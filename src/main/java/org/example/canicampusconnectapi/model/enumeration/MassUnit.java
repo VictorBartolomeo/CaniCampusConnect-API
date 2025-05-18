cfb688e16ac2cc565c6c662fe3970ef48836aa07
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-18 13:27:54 +0200

    Add @JsonView annotations to enumeration classes for OwnerViewDog serialization.

--- src/main/java/org/example/canicampusconnectapi/model/enumeration/MassUnit.java
+++ src/main/java/org/example/canicampusconnectapi/model/enumeration/MassUnit.java
@@ -1,5 +1,9 @@
 package org.example.canicampusconnectapi.model.enumeration;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import org.example.canicampusconnectapi.view.owner.OwnerViewDog;
+
+@JsonView(OwnerViewDog.class)
 public enum MassUnit {
     KILOGRAM,
     POUND,
--- src/main/java/org/example/canicampusconnectapi/model/enumeration/RegistrationStatus.java
+++ src/main/java/org/example/canicampusconnectapi/model/enumeration/RegistrationStatus.java
@@ -1,5 +1,8 @@
 package org.example.canicampusconnectapi.model.enumeration;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import org.example.canicampusconnectapi.view.owner.OwnerViewDog;
+
 /**
  * Represents the status of a registration for a course.
  * The workflow is as follows:
@@ -7,6 +10,7 @@ package org.example.canicampusconnectapi.model.enumeration;
  * 2. A club owner or coach must validate the registration to change it to CONFIRMED
  * 3. A registration can be CANCELLED at any time by the owner, coach, or club owner
  */
+@JsonView(OwnerViewDog.class)
 public enum RegistrationStatus {
     CONFIRMED,
     PENDING,
