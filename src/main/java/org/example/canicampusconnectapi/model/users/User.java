9768ea20798b47c1a01692f95bb909b35e6e406f
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-21 09:53:03 +0200

    Refactor user model to update validation groups and add write-only property for password.

--- src/main/java/org/example/canicampusconnectapi/model/users/User.java
+++ src/main/java/org/example/canicampusconnectapi/model/users/User.java
@@ -1,6 +1,7 @@
 package org.example.canicampusconnectapi.model.users;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
@@ -22,9 +23,6 @@ public class User {
     }
     public interface OnUpdatePassword {}
 
-    public interface OnCreate {
-    }
-
     private static final String REGEX_STRONG_PASSWORD =
             "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*\\W).{8,64}$";
 
@@ -34,23 +32,24 @@ public class User {
     protected Long id;
 
     @Column(nullable = false, unique = true, length = 150)
-    @NotBlank(message = "L'email ne peut pas être vide", groups = {OnCreate.class, OnUpdateFromOwner.class})
+    @NotBlank(message = "L'email ne peut pas être vide", groups = {Owner.onCreateOwner.class, OnUpdateFromOwner.class})
     @Email(message = "L'email n'est pas au format valide")
     @JsonView({OwnerViewDog.class, OwnerView.class})
     protected String email;
 
     @Column(nullable = false, length = 100)
-    @NotBlank(message = "Le prénom ne peut pas être vide", groups = {OnCreate.class, OnUpdateFromOwner.class})
+    @NotBlank(message = "Le prénom ne peut pas être vide", groups = {Owner.onCreateOwner.class, OnUpdateFromOwner.class})
     @JsonView({OwnerViewDog.class, OwnerView.class})
     protected String firstname;
 
     @Column(nullable = false, length = 100)
-    @NotBlank(message = "Le nom de famille ne peut pas être vide", groups = {OnCreate.class, OnUpdateFromOwner.class})
+    @NotBlank(message = "Le nom de famille ne peut pas être vide", groups = {Owner.onCreateOwner.class, OnUpdateFromOwner.class})
     @JsonView({OwnerViewDog.class, OwnerView.class})
     protected String lastname;
 
     @Column(nullable = false, length = 255) //taille de 255 caractères pour prévenir le mot de passe hashé
-    @NotBlank(message = "Le mot de passe ne peut pas être vide", groups = {OnCreate.class, OnUpdatePassword.class})
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotBlank(message = "Le mot de passe ne peut pas être vide", groups = {Owner.onCreateOwner.class, OnUpdatePassword.class})
     @Size(min = 8, max = 64, message = "Le mot de passe doit contenir entre 8 et 64 caractères")
     @Pattern(regexp = REGEX_STRONG_PASSWORD, message = "Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial")
     protected String password;
