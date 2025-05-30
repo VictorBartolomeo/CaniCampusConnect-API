3a5d93319e0fa17f6729d2393db3ea2de862156d
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-30 16:59:01 +0200

    Add CoachViewRegistrations to JsonView annotations in RegistrationStatus enum

--- src/main/java/org/example/canicampusconnectapi/model/enumeration/RegistrationStatus.java
+++ src/main/java/org/example/canicampusconnectapi/model/enumeration/RegistrationStatus.java
@@ -1,6 +1,7 @@
 package org.example.canicampusconnectapi.model.enumeration;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import org.example.canicampusconnectapi.view.coach.CoachViewRegistrations;
 import org.example.canicampusconnectapi.view.owner.OwnerViewCourse;
 import org.example.canicampusconnectapi.view.owner.OwnerViewDog;
 
@@ -11,7 +12,7 @@ import org.example.canicampusconnectapi.view.owner.OwnerViewDog;
  * 2. A club owner or coach must validate the registration to change it to CONFIRMED
  * 3. A registration can be CANCELLED at any time by the owner, coach, or club owner
  */
-@JsonView({OwnerViewDog.class, OwnerViewCourse.class})
+@JsonView({OwnerViewDog.class, OwnerViewCourse.class, CoachViewRegistrations.class})
 public enum RegistrationStatus {
     CONFIRMED,
     PENDING,
