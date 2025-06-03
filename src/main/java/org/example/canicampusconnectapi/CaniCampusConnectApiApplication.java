461b8a2b14fa3fcbe0598c41eed6aa6e194a7deb
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-06-03 13:42:04 +0200

    Enable scheduling in the application configuration

--- src/main/java/org/example/canicampusconnectapi/CaniCampusConnectApiApplication.java
+++ src/main/java/org/example/canicampusconnectapi/CaniCampusConnectApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -12,6 +13,7 @@ import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableScheduling
 public class CaniCampusConnectApiApplication {
 
     public static void main(String[] args) {
