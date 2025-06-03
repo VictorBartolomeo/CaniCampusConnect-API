728314bde12406710f1fb90f6c4f2057312e6646
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-06-03 16:18:00 +0200

    Expand BreedService to include CRUD operations and implement them in BreedServiceImpl.

--- src/main/java/org/example/canicampusconnectapi/service/breed/BreedService.java
+++ src/main/java/org/example/canicampusconnectapi/service/breed/BreedService.java
@@ -1,11 +1,15 @@
 package org.example.canicampusconnectapi.service.breed;
 
 import org.example.canicampusconnectapi.model.dogRelated.Breed;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface BreedService {
-
-    Optional<Breed> updateAvatarUrl(Short breedId, String avatarUrl);
     Optional<Breed> findById(Short id);
-    Optional<Breed> findByName(String name);
+    List<Breed> findAll();
+    Breed save(Breed breed);
+    void deleteById(Short id);
+    boolean existsById(Short id);
+    Optional<Breed> updateAvatarUrl(Short breedId, String avatarUrl);
 }
--- src/main/java/org/example/canicampusconnectapi/service/breed/BreedServiceImpl.java
+++ src/main/java/org/example/canicampusconnectapi/service/breed/BreedServiceImpl.java
@@ -7,6 +7,7 @@ import org.example.canicampusconnectapi.model.dogRelated.Breed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,12 +16,36 @@ public class BreedServiceImpl implements BreedService {
 
     private final BreedDao breedDao;
 
-    // Constructeur pour l'injection de dépendance
-    @Autowired
     public BreedServiceImpl(BreedDao breedDao) {
         this.breedDao = breedDao;
     }
 
+    @Override
+    public Optional<Breed> findById(Short id) {
+        return breedDao.findByIdWithDogs(id);
+    }
+
+    @Override
+    public List<Breed> findAll() {
+        return breedDao.findAllWithDogs();
+    }
+
+    @Override
+    public Breed save(Breed breed) {
+        return breedDao.save(breed);
+    }
+
+    @Override
+    public void deleteById(Short id) {
+        breedDao.deleteById(id);
+    }
+
+    @Override
+    public boolean existsById(Short id) {
+        return breedDao.existsById(id);
+    }
+
+    @Override
     public Optional<Breed> updateAvatarUrl(Short breedId, String avatarUrl) {
         return breedDao.findById(breedId)
                 .map(breed -> {
