e9d319a6ac4029e7b61e3c62f5fcac5a75706ca5
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-06-11 21:58:34 +0200

    Comment out `FileService` class to disable unused file handling implementation.

--- src/main/java/org/example/canicampusconnectapi/service/FileService.java
+++ src/main/java/org/example/canicampusconnectapi/service/FileService.java
@@ -1,58 +1,58 @@
-package org.example.canicampusconnectapi.service;
-
-import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-
-@Service
-public class FileService {
-
-    @Value("${public.upload.folder}")
-    private String publicUploadFolder;
-
-    @Value("${private.upload.folder}")
-    private String privateUploadFolder;
-
-    public void uploadToLocalFileSystem(MultipartFile file, String fileName, boolean publicFile) throws IOException {
-        uploadToLocalFileSystem(file.getInputStream(), fileName, publicFile);
-    }
-
-    public void uploadToLocalFileSystem(InputStream inputStream, String fileName, boolean publicFile) throws IOException {
-
-        Path storageDirectory = Paths.get(publicFile ? publicUploadFolder : privateUploadFolder);
-
-        if (!Files.exists(storageDirectory)) {
-            try {
-                Files.createDirectories(storageDirectory);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        Path destination = Paths.get(storageDirectory.toString() + "/" + fileName);
-
-        Files.copy(inputStream, destination, StandardCopyOption.REPLACE_EXISTING);
-
-    }
-
-    public byte[] getImageByName(String nomImage) throws FileNotFoundException {
-
-        Path destination = Paths.get(privateUploadFolder + "/" + nomImage);// retrieve the image by its name
-
-        try {
-            return IOUtils.toByteArray(destination.toUri());
-        } catch (IOException e) {
-            throw new FileNotFoundException(e.getMessage());
-        }
-
-    }
-}
+//package org.example.canicampusconnectapi.service;
+//
+//import org.apache.commons.io.IOUtils;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.io.FileNotFoundException;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.nio.file.StandardCopyOption;
+//
+//@Service
+//public class FileService {
+//
+//    @Value("${public.upload.folder}")
+//    private String publicUploadFolder;
+//
+//    @Value("${private.upload.folder}")
+//    private String privateUploadFolder;
+//
+//    public void uploadToLocalFileSystem(MultipartFile file, String fileName, boolean publicFile) throws IOException {
+//        uploadToLocalFileSystem(file.getInputStream(), fileName, publicFile);
+//    }
+//
+//    public void uploadToLocalFileSystem(InputStream inputStream, String fileName, boolean publicFile) throws IOException {
+//
+//        Path storageDirectory = Paths.get(publicFile ? publicUploadFolder : privateUploadFolder);
+//
+//        if (!Files.exists(storageDirectory)) {
+//            try {
+//                Files.createDirectories(storageDirectory);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        Path destination = Paths.get(storageDirectory.toString() + "/" + fileName);
+//
+//        Files.copy(inputStream, destination, StandardCopyOption.REPLACE_EXISTING);
+//
+//    }
+//
+//    public byte[] getImageByName(String nomImage) throws FileNotFoundException {
+//
+//        Path destination = Paths.get(privateUploadFolder + "/" + nomImage);// retrieve the image by its name
+//
+//        try {
+//            return IOUtils.toByteArray(destination.toUri());
+//        } catch (IOException e) {
+//            throw new FileNotFoundException(e.getMessage());
+//        }
+//
+//    }
+//}
