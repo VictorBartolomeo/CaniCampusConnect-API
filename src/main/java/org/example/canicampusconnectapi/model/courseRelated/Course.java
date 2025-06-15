c6ed6383008f805674a39ccdd97e092a750dc127
Victor Monteragioni <victor.monteriggioni@gmail.com>
2025-06-15 03:02:28 +0200

    Refactor `Course` model to adjust spacing, enhance readability, add default club handling with `@PrePersist` and `@PreUpdate` methods, and update annotations.

--- src/main/java/org/example/canicampusconnectapi/model/courseRelated/Course.java
+++ src/main/java/org/example/canicampusconnectapi/model/courseRelated/Course.java
@@ -35,7 +35,7 @@ import java.util.List;
 @EntityListeners(AuditingEntityListener.class)
 public class Course {
 
-    public interface CreateCourse{
+    public interface CreateCourse {
 
     }
 
@@ -46,48 +46,50 @@ public class Course {
     protected Long id;
 
     @Column(nullable = false, length = 255)
-    @NotBlank(message = "Le titre ne peut pas être vide", groups ={CreateCourse.class})
-    @JsonView({OwnerViewDog.class,OwnerViewCourse.class,CoachView.class,CoachViewRegistrations.class, CourseTypeView.class})
+    @NotBlank(message = "Le titre ne peut pas être vide", groups = {CreateCourse.class})
+    @JsonView({OwnerViewDog.class, OwnerViewCourse.class, CoachView.class, CoachViewRegistrations.class, CourseTypeView.class})
     protected String title;
 
     @Column(columnDefinition = "TEXT")
-    @JsonView({OwnerViewDog.class,OwnerViewCourse.class,CoachView.class,CoachViewRegistrations.class})
+    @JsonView({OwnerViewDog.class, OwnerViewCourse.class, CoachView.class, CoachViewRegistrations.class})
     protected String description;
 
     @Column(nullable = false)
-    @NotNull(message = "La date et l'heure de début ne peuvent pas être vides", groups ={CreateCourse.class})
-    @JsonView({OwnerViewDog.class,OwnerViewCourse.class, CoachView.class,CoachViewRegistrations.class})
+    @NotNull(message = "La date et l'heure de début ne peuvent pas être vides", groups = {CreateCourse.class})
+    @JsonView({OwnerViewDog.class, OwnerViewCourse.class, CoachView.class, CoachViewRegistrations.class})
     protected Instant startDatetime;
 
     @Column(nullable = false)
-    @NotNull(message = "La date et l'heure de fin ne peuvent pas être vides", groups ={CreateCourse.class})
-    @JsonView({OwnerViewDog.class,OwnerViewCourse.class, CoachView.class,CoachViewRegistrations.class})
+    @NotNull(message = "La date et l'heure de fin ne peuvent pas être vides", groups = {CreateCourse.class})
+    @JsonView({OwnerViewDog.class, OwnerViewCourse.class, CoachView.class, CoachViewRegistrations.class})
     protected Instant endDatetime;
 
     @Column(nullable = false)
-    @NotNull(message = "Vous devez renseigner un nombre de place limitées", groups ={CreateCourse.class})
-    @JsonView({OwnerViewDog.class,OwnerViewCourse.class,CoachView.class,CoachViewRegistrations.class})
+    @NotNull(message = "Vous devez renseigner un nombre de place limitées", groups = {CreateCourse.class})
+    @JsonView({OwnerViewDog.class, OwnerViewCourse.class, CoachView.class, CoachViewRegistrations.class})
     protected int maxCapacity;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
-    @NotNull(message="Vous devez renseigner un coach", groups ={CreateCourse.class})
-    @JsonView({OwnerViewDog.class,OwnerViewCourse.class,CoachViewRegistrations.class})
+    @NotNull(message = "Vous devez renseigner un coach", groups = {CreateCourse.class})
+    @JsonView({OwnerViewDog.class, OwnerViewCourse.class, CoachViewRegistrations.class})
     protected Coach coach;
 
+    //Sera toujours 1 via le PrePersist et le PreUpdate, je ne gère pas encore les multiples clubs
     @ManyToOne
     @JoinColumn(name = "club_id", nullable = false)
-    @NotNull(message="Vous devez renseigner un club", groups ={CreateCourse.class})
+    @NotNull(message = "Vous devez renseigner un club") //Retiré de l'interface validated OnCreateCourse
+    @JsonIgnore //Le club ne m'interesse pas pour le moment car pas de gestion de clubs multiples
     protected Club club;
 
     @ManyToOne
     @JoinColumn(name = "course_type_id", nullable = false)
-    @NotNull(message="Vous devez renseigner un type de cours", groups ={CreateCourse.class})
-    @JsonView({OwnerViewDog.class,OwnerViewCourse.class,CoachView.class,CoachViewRegistrations.class})
+    @NotNull(message = "Vous devez renseigner un type de cours", groups = {CreateCourse.class})
+    @JsonView({OwnerViewDog.class, OwnerViewCourse.class, CoachView.class, CoachViewRegistrations.class})
     protected CourseType courseType;
 
     @OneToMany(mappedBy = "course")
-    @JsonView({OwnerViewCourse.class,CoachView.class})
+    @JsonView({CoachView.class})
     protected List<Registration> registrations;
 
     @CreatedDate
@@ -109,4 +111,21 @@ public class Course {
     @Column(nullable = false, length = 150)
     @JsonView({OwnerViewCourse.class, CoachView.class, AdminViewCourse.class})
     private String lastModifiedBy;
+
+    @PrePersist
+    private void setDefaultClub() {
+        if (this.club == null) {
+            Club defaultClub = new Club();
+            defaultClub.setId(1);
+            this.club = defaultClub;
+        }
+    }
+
+    @PreUpdate
+    private void ensureDefaultClub() {
+        Club defaultClub = new Club();
+        defaultClub.setId(1);
+        this.club = defaultClub;
+    }
+
 }
