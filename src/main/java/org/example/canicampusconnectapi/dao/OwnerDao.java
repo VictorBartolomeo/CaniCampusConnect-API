385dab55bd397c97703a0b1d13f5d87359d1b5ac
Victor Monteragioni <victor.monteriggioni@gmail.com>
2025-08-03 23:33:18 +0200

    Add queries to fetch and count non-anonymized owners in `OwnerDao`.

--- src/main/java/org/example/canicampusconnectapi/dao/OwnerDao.java
+++ src/main/java/org/example/canicampusconnectapi/dao/OwnerDao.java
@@ -2,8 +2,10 @@ package org.example.canicampusconnectapi.dao;
 
 import org.example.canicampusconnectapi.model.users.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -11,5 +13,12 @@ import java.util.Optional;
 public interface OwnerDao extends JpaRepository<Owner, Long> {
 
     Optional<Owner> findByEmail(String email);
+
+    @Query("SELECT o FROM Owner o WHERE o.isAnonymized = false")
+    List<Owner> findAllNotAnonymized();
+
+    @Query("SELECT COUNT(o) FROM Owner o WHERE o.isAnonymized = false")
+    long countNotAnonymized();
+
 
 }
