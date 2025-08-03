91de8644a00991dc6d902e42ef6f17e2d13b0839
Victor Monteragioni <victor.monteriggioni@gmail.com>
2025-08-03 23:33:40 +0200

    Add `RegistrationFactory` to encapsulate `Registration` object creation logic.

---
+++ src/main/java/org/example/canicampusconnectapi/factory/RegistrationFactory.java
@@ -0,0 +1,22 @@
+package org.example.canicampusconnectapi.factory;
+
+import org.example.canicampusconnectapi.model.courseRelated.Registration;
+import org.example.canicampusconnectapi.model.enumeration.RegistrationStatus;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
+public class RegistrationFactory {
+
+    /**
+     * Encapsule la création d'objets complexes.
+     */
+    public Registration prepareNewRegistration(Registration registration) {
+        Registration newRegistration = new Registration();
+        newRegistration.setCourse(registration.getCourse());
+        newRegistration.setDog(registration.getDog());
+        newRegistration.setStatus(RegistrationStatus.PENDING);
+        return newRegistration;
+    }
+}
