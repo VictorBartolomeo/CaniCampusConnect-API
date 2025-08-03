63690ebe9977f1bba129352f0d28c67514d1c7c1
Victor Monteragioni <victor.monteriggioni@gmail.com>
2025-08-03 23:31:53 +0200

    Add `BusinessException` class for custom runtime exception handling

---
+++ src/main/java/org/example/canicampusconnectapi/common/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package org.example.canicampusconnectapi.common.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
