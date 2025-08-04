ac359a1282550a99a59cb161d2850cca99650155
Victor Monteragioni <victor.monteriggioni@gmail.com>
2025-08-04 16:22:52 +0200

    Update `@IsClubOwner` annotation to use `hasRole` instead of `hasAnyRole` for specificity and correctness.

--- src/main/java/org/example/canicampusconnectapi/security/annotation/role/IsClubOwner.java
+++ src/main/java/org/example/canicampusconnectapi/security/annotation/role/IsClubOwner.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@PreAuthorize("hasAnyRole('ROLE_CLUB_OWNER')")
+@PreAuthorize("hasRole('ROLE_CLUB_OWNER')")
 public @interface IsClubOwner {
 
 }
